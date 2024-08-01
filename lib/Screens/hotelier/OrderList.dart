153023a48c184ffcdbd1037cee1e88bab1cfa48e
tienvu739 <tienvu739zx@gmail.com>
2024-08-01 20:51:42 +0700

    update 6

--- lib/Screens/hotelier/OrderList.dart
+++ lib/Screens/hotelier/OrderList.dart
@@ -38,6 +38,7 @@ class _OrderPageState extends State<OrderPage> {
         setState(() {
           orders = responseData.map<Map<String, dynamic>>((order) {
             return {
+              'id': order['id'],
               'dateCreated': order['dateCreated'],
               'checkInDate': order['checkInDate'],
               'checkOutDate': order['checkOutDate'],
@@ -55,6 +56,38 @@ class _OrderPageState extends State<OrderPage> {
     }
   }
 
+  Future<void> _deleteOrder(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('auth_token') ?? '';
+
+    final url = Uri.parse('https://10.0.2.2:7226/api/Order/delete?id=$id');
+    try {
+      final response = await http.delete(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          orders.removeWhere((order) => order['id'] == id);
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Đơn hàng đã được xóa.')),
+        );
+      } else {
+        print('Failed to delete order: ${response.body}');
+      }
+    } catch (error) {
+      print('Error: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Đã xảy ra lỗi: $error')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,40 +100,47 @@ class _OrderPageState extends State<OrderPage> {
         padding: const EdgeInsets.all(16.0),
         child: orders.isEmpty
             ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: orders.length,
-                itemBuilder: (context, index) {
-                  final order = orders[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(
-                        'Khách sạn: ${order['hotelName']}',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+            : RefreshIndicator(
+          onRefresh: fetchOrders,
+          child: ListView.builder(
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              final order = orders[index];
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    'Khách sạn: ${order['hotelName']}',
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Phòng: ${order['roomName']}',
+                        style: TextStyle(fontSize: 15),
                       ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Phòng: ${order['roomName']}',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          Text('Ngày tạo: ${order['dateCreated']}',
-                              style: TextStyle(fontSize: 15)),
-                          Text('Ngày nhận phòng: ${order['checkInDate']}',
-                              style: TextStyle(fontSize: 15)),
-                          Text('Ngày trả phòng: ${order['checkOutDate']}',
-                              style: TextStyle(fontSize: 15)),
-                          Text(
-                            'Giá: ${order['price']} VND',
-                            style: TextStyle(color: Colors.red, fontSize: 15),
-                          ),
-                        ],
+                      Text('Ngày tạo: ${order['dateCreated']}',
+                          style: TextStyle(fontSize: 15)),
+                      Text('Ngày nhận phòng: ${order['checkInDate']}',
+                          style: TextStyle(fontSize: 15)),
+                      Text('Ngày trả phòng: ${order['checkOutDate']}',
+                          style: TextStyle(fontSize: 15)),
+                      Text(
+                        'Giá: ${order['price']} VND',
+                        style: TextStyle(color: Colors.red, fontSize: 15),
                       ),
-                    ),
-                  );
-                },
-              ),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteOrder(order['id']),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
